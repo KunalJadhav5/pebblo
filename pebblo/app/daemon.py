4ab162f07b6b6f14e6b6097ecc45e5437495dde2
Kunal Jadhav <47965366+KunalJadhav5@users.noreply.github.com>
2024-02-08 16:47:57 +0530

    Added config file changes for logger and enums

--- pebblo/app/daemon.py
+++ pebblo/app/daemon.py
@@ -1,36 +1,29 @@
-from pebblo.topic_classifier.topic_classifier import TopicClassifier
-from pebblo.entity_classifier.entity_classifier import EntityClassifier
-
 from pebblo.app.config.config import load_config
 import sys
 import os
 import argparse
 
-from pebblo.app.config.service import Service
 
 config_details = {}
 
 
 def start():
-    # CLI input details
-    cli_input = list(sys.argv)
-    cli_str = ' '.join(cli_input)
     global config_details
-
     # For loading config file details
     parser = argparse.ArgumentParser(description="Pebblo  CLI")
-    parser.add_argument('s', type=str, help="Config file path")
+    parser.add_argument('--config', type=str, help="Config file path")
     args = parser.parse_args()
     path = args.config
     config_details = load_config(path)
-    print(f'----- Config Details for pebblo {config_details} -----')
-    log_level = config_details.get('logging', {}).get('level', 'info')
-    os.environ.setdefault("PEBBLO_LOG_LEVEL", log_level)
+
+    from pebblo.topic_classifier.topic_classifier import TopicClassifier
+    from pebblo.entity_classifier.entity_classifier import EntityClassifier
     # Init TopicClassifier(This step downloads the models and put in cache)
     _ = TopicClassifier()
     # Init EntityClassifier(This step downloads all necessary training models)
     _ = EntityClassifier()
 
     # Starting Uvicorn Service Using config details
+    from pebblo.app.config.service import Service
     svc = Service(config_details)
     svc.start()
--- pebblo/app/libs/logger.py
+++ pebblo/app/libs/logger.py
@@ -6,15 +6,14 @@ import os
 from pebblo.app.daemon import config_details
 
 
-def get_logger(log_level):
+def get_logger():
     """Get object of logger"""
-    print('-----logger details for pebblo-----')
-    print(os.environ.get('PEBBLO_LOG_LEVEL', 'ABC'))
+    logger_level = config_details.get('logging', {}).get('level', 'info').upper()
     logger_obj = logging.getLogger("Pebblo Logger")
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     console_handle = logging.StreamHandler()
     console_handle.setFormatter(formatter)
-    logger_obj.setLevel(os.environ.get('LOG_LEVEL', 'INFO'))
+    logger_obj.setLevel(logger_level)
     logger_obj.propagate = False
     if not logger_obj.handlers:
         logger_obj.addHandler(console_handle)
