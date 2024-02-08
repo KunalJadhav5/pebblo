0f055bae6f78d27ede6cb240cd37277381ee2c63
Kunal Jadhav <47965366+KunalJadhav5@users.noreply.github.com>
2024-02-08 22:21:22 +0530

    Added comment and updated config.yaml

--- pebblo/app/daemon.py
+++ pebblo/app/daemon.py
@@ -16,6 +16,7 @@ def start():
     path = args.config
     config_details = load_config(path)
 
+    # LazyLoading TopicClassifier and EntityClassifier to avoid circular dependency
     from pebblo.topic_classifier.topic_classifier import TopicClassifier
     from pebblo.entity_classifier.entity_classifier import EntityClassifier
     # Init TopicClassifier(This step downloads the models and put in cache)
