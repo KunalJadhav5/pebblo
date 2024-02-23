9500833f19732bfa717da4851c215bbc7b959efe
Kunal Jadhav <47965366+KunalJadhav5@users.noreply.github.com>
2024-02-23 12:58:36 +0530

    Updated local

--- pebblo/app/service/local_ui_service.py
+++ pebblo/app/service/local_ui_service.py
@@ -9,76 +9,88 @@ from pebblo.app.models.models import AppListDetails, AppModel
 class AppData:
     @staticmethod
     def get_all_apps_details():
-        """
-        Returns all necessary app details required for listing.
-        """
+        """Returns all necessary app details required for listing."""
         try:
-            # Retrieve the full path of the cache directory
             dir_full_path = get_full_path(CacheDir.home_dir.value)
             # List all apps in the directory
             dir_path = os.listdir(dir_full_path)
 
-            # Initialize lists and counters
+            # Default values
             all_apps = []
-            apps_at_risk = findings = files_findings = data_source = 0
+            apps_at_risk = 0
+            findings = 0
+            files_findings = 0
+            data_source = 0
             findings_list = []
             data_source_list = []
             document_with_findings_list = []
 
-            # Iterate through each app in the directory
-            for app_dir in os.listdir(dir_full_path):
+            # Iterating through each app in the directory
+            for app_dir in dir_path:
                 if app_dir.startswith('.'):
                     # Skip hidden folders
                     logger.debug(f'Skipping hidden folder {app_dir}')
                     continue
-
                 # Path to metadata.json
-                app_path = os.path.join(dir_full_path, app_dir, CacheDir.metadata_file_path.value)
+                app_path = f'{CacheDir.home_dir.value}/{app_dir}/{CacheDir.metadata_file_path.value}'
+                logger.debug(f'metadata.json path {app_path}')
                 app_json = read_json_file(app_path)
-
-                if app_json and app_json.get('load_ids'):
-                    latest_load_id = app_json.get('load_ids')[-1]
-                    app_detail_path = os.path.join(dir_full_path, app_dir, latest_load_id, CacheDir.report_data_file_name.value)
-                    app_detail_json = read_json_file(app_detail_path)
-
-                    if app_detail_json:
-                        report_summary = app_detail_json.get('reportSummary')
-                        app_name = app_json.get('name')
-                        app_details = AppListDetails(
-                            name=app_name,
-                            topics=report_summary.get('findingsTopics', 0),
-                            entities=report_summary.get('findingsEntities', 0),
-                            owner=report_summary.get('owner'),
-                            loadId=latest_load_id
-                        )
-
-                        data_source_details = app_detail_json.get('dataSources')
-                        if data_source_details:
-                            for data in data_source_details:
-                                updated_data_source_dict = update_data_source(data, app_name)
-                                data_source_list.append(updated_data_source_dict)
-                                finding_data = update_findings_summary(data, app_name)
-                                findings_list.extend(finding_data)
-
-                        app_metadata_detail_path = os.path.join(dir_full_path, app_dir, latest_load_id, CacheDir.metadata_file_path.value)
-                        app_metadata_json_details = read_json_file(app_metadata_detail_path)
-                        documents_with_findings_data = get_document_with_findings_data(app_metadata_json_details)
-                        document_with_findings_list.extend(documents_with_findings_data)
-
-                        # Update counters
-                        findings += report_summary.get('findings', 0)
-                        files_findings += report_summary.get('filesWithFindings', 0)
-                        data_source += report_summary.get('dataSources', 0)
-                        if report_summary.get('findings', 0) > 0:
-                            apps_at_risk += 1
-
-                        # Add app details to the list
-                        all_apps.append(app_details.dict())
-                else:
+                # Condition for handling loadId
+                if not app_json and not app_json.get('load_ids'):
+                    # Unable to fetch LoadId Details
                     logger.debug('Error: Unable to fetch loadId details')
                     logger.debug(f'App Json : {app_json}')
+                    continue
+                    # Fetching latest loadId
+                latest_load_id = app_json.get("load_ids")[-1]
+                # Path to report.json
+                app_detail_path = f'{CacheDir.home_dir.value}/{app_dir}/{latest_load_id}/{CacheDir.report_data_file_name.value}'
+                logger.debug(f'report.json path {app_detail_path}')
+                app_detail_json = read_json_file(app_detail_path)
+                if not app_detail_json:
+                    logger.debug('Error: Unable to fetch loadId details')
+                    logger.debug(f'App Json : {app_json}')
+                    continue
+                report_summary = app_detail_json.get('reportSummary')
+                app_name = app_json.get('name')
+                app_details = AppListDetails(
+                    name=app_json.get('name'),
+                    topics=report_summary.get('findingsTopics', 0),
+                    entities=report_summary.get('findingsEntities', 0),
+                    owner=report_summary.get('owner'),
+                    loadId=latest_load_id
+                )
 
-            # Create AppModel instance with collected data
+                # Fetching Details for dashboard tabs
+                data_source_details = app_detail_json.get('dataSources')
+                # Fetching only required values for dashboard pages
+                if data_source_details:
+                    for data in data_source_details:
+                        # Adding appName in dataSource
+                        updated_data_source_dict = update_data_source(data, app_name)
+                        data_source_list.append(updated_data_source_dict)
+                        # Adding appName in findingsSummary
+                        finding_data = update_findings_summary(data, app_name)
+                        # appending only required value for dashboard
+                        findings_list.extend(finding_data)
+
+                # Fetching DocumentWithFindings details from app metadata.json
+                app_metadata_detail_path = f'{CacheDir.home_dir.value}/{app_dir}/{latest_load_id}/{CacheDir.metadata_file_path.value}'
+                app_metadata_json_details = read_json_file(app_metadata_detail_path)
+                # Fetching required data for DocumentWithFindings
+                documents_with_findings_data = get_document_with_findings_data(app_metadata_json_details)
+                document_with_findings_list.extend(documents_with_findings_data)
+
+                # Dashboard Counts
+                findings += report_summary.get('findings', 0)
+                files_findings += report_summary.get('filesWithFindings', 0)
+                data_source += report_summary.get('dataSources', 0)
+                if report_summary.get('findings', 0) > 0:
+                    apps_at_risk += 1
+
+                all_apps.append(app_details.dict())
+
+            # Validation
             data = AppModel(
                 applicationsAtRiskCount=apps_at_risk,
                 findingsCount=findings,
@@ -89,28 +101,36 @@ class AppData:
                 documentsWithFindings=document_with_findings_list,
                 dataSource=data_source_list
             )
-            print(f'---data{data.dict()}---')
-            # Return JSON representation of AppModel instance
+
             return json.dumps(data.dict(), indent=4)
 
         except Exception as ex:
-            logger.error(f"Error in Dashboard app Listing. Error: {ex}")
+            logger.error(f"Error in Dashboard app Listing. Error:{ex}")
 
     @staticmethod
     def get_app_details(app_dir):
         try:
             # Path to metadata.json
-            app_path = os.path.join(CacheDir.home_dir.value, app_dir, CacheDir.metadata_file_path.value)
+            app_path = f'{CacheDir.home_dir.value}/{app_dir}/{CacheDir.metadata_file_path.value}'
+            logger.debug(f'metadata.json path {app_path}')
+            # Reading metadata.json
             app_json = read_json_file(app_path)
-
-            if app_json and app_json.get('load_ids'):
+            # Condition for handling loadId
+            if app_json and app_json.get('load_ids') is not None and len(app_json.get('load_ids')) > 0:
+                # Fetching latest loadId
                 latest_load_id = app_json.get('load_ids')[-1]
-                app_detail_path = os.path.join(CacheDir.home_dir.value, app_dir, latest_load_id, CacheDir.report_data_file_name.value)
+
+                # Path to report.json
+                app_detail_path = f'{CacheDir.home_dir.value}/{app_dir}/{latest_load_id}/{CacheDir.report_data_file_name.value}'
+                logger.debug(f'metadata.json path {app_detail_path}')
+
+                # Reading report.json
                 app_detail_json = read_json_file(app_detail_path)
                 if app_detail_json:
                     return json.dumps(app_detail_json, indent=4)
                 else:
                     return json.dumps({})
+
             else:
                 logger.debug('Error: Unable to fetch loadId details')
                 logger.debug(f'App Json : {app_json}')
