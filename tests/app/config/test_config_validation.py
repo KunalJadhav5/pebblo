0594e619e7fda1998e98c21df6859bc8118cdf00
Kunal Jadhav <47965366+KunalJadhav5@users.noreply.github.com>
2024-03-05 23:42:30 +0530

    added test case

--- tests/app/config/test_config_validation.py
+++ tests/app/config/test_config_validation.py
@@ -13,28 +13,30 @@ def config_dict():
 
 
 def test_validate_config(config_dict):
-    with patch('pebblo.app.config.config_validation.get_full_path', return_value='~/.pebblo'):
-        with patch('pebblo.app.config.config_validation.logger.error'):
+    with patch("pebblo.app.config.config_validation.get_full_path", return_value="~/.pebblo"):
+        with patch("pebblo.app.config.config_validation.logger.error") as mock_logger_error:
             with pytest.raises(SystemExit):
                 validate_config(config_dict)
+            assert mock_logger_error.call_count == 0
 
 
-def test_daemon_config_validate():
-    config = {"host": "localhost", "port": 8000}
+def test_daemon_config_validate(config_dict):
+    print(config_dict)
+    config = config_dict.get("daemon")
     daemon_config = DaemonConfig(config)
     daemon_config.validate()
     assert not daemon_config.errors
 
 
-def test_logging_config_validate():
-    config = {"level": "info"}
+def test_logging_config_validate(config_dict):
+    config = config_dict.get("logging")
     logging_config = LoggingConfig(config)
     logging_config.validate()
     assert not logging_config.errors
 
 
-def test_reports_config_validate():
-    config = {"format": "pdf", "renderer": "xhtml2pdf", "outputDir": "~/.pebblo"}
+def test_reports_config_validate(config_dict):
+    config = config_dict.get("reports")
     reports_config = ReportsConfig(config)
     reports_config.validate()
     assert not reports_config.errors
