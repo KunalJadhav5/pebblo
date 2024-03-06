d2d7e4802a389072d0064448d21ddd8af7d5aeb9
Kunal Jadhav <47965366+KunalJadhav5@users.noreply.github.com>
2024-03-06 13:17:59 +0530

    Fixed import

--- tests/app/config/test_config_validation.py
+++ tests/app/config/test_config_validation.py
@@ -1,23 +1,23 @@
-from pebblo.app.config import config_validate
+from pebblo.app.config.config_validation import validate_config
 import pytest
 
 
 def test_daemon_config_validate():
     # Test with valid host and port
     config = {"host": "localhost", "port": "8000"}
-    validator = config_validate.DaemonConfig(config)
+    validator = validate_config.DaemonConfig(config)
     validator.validate()
     assert validator.errors == []
 
     # Test with invalid host
     config = {"host": 123, "port": "8000"}
-    validator = config_validate.DaemonConfig(config)
+    validator = validate_config.DaemonConfig(config)
     validator.validate()
     assert validator.errors == ["Error: Invalid host '123'. Host must be a string."]
 
     # Test with invalid port
     config = {"host": "localhost", "port": "70000"}
-    validator = config_validate.DaemonConfig(config)
+    validator = validate_config.DaemonConfig(config)
     validator.validate()
     assert validator.errors == [
         "Error: Invalid port '70000'. Port must be between 1 and 65535."
@@ -25,7 +25,7 @@ def test_daemon_config_validate():
 
     # Test with invalid port using string value
     config = {"host": "localhost", "port": "abc"}
-    validator = config_validate.DaemonConfig(config)
+    validator = validate_config.DaemonConfig(config)
     validator.validate()
     assert validator.errors == [
         "Error: Invalid port value 'abc'. Port must be an integer."
@@ -35,14 +35,14 @@ def test_daemon_config_validate():
 def test_logging_config_validate():
     # Test with valid log level
     config = {"level": "info"}
-    validator = config_validate.LoggingConfig(config)
+    validator = validate_config.LoggingConfig(config)
     validator.validate()
     assert validator.errors == []
 
     # Test with invalid log level
     log_level = "invalid_level"
     config = {"level": log_level}
-    validator = config_validate.LoggingConfig(config)
+    validator = validate_config.LoggingConfig(config)
     validator.validate()
     assert validator.errors == [
         f"Error: Unsupported logLevel '{log_level.upper()}' specified in the configuration"
@@ -52,13 +52,13 @@ def test_logging_config_validate():
 def test_reports_config_validate():
     # Test with valid format, renderer, and output directory
     config = {"format": "pdf", "renderer": "xhtml2pdf", "outputDir": "~/.pebblo"}
-    validator = config_validate.ReportsConfig(config)
+    validator = validate_config.ReportsConfig(config)
     validator.validate()
     assert validator.errors == []
 
     # Test with invalid format
     config = {"format": "doc", "renderer": "xhtml2pdf", "outputDir": "~/.pebblo"}
-    validator = config_validate.ReportsConfig(config)
+    validator = validate_config.ReportsConfig(config)
     validator.validate()
     assert validator.errors == [
         "Error: Unsupported format 'doc' specified in the configuration"
@@ -66,7 +66,7 @@ def test_reports_config_validate():
 
     # Test with invalid renderer
     config = {"format": "pdf", "renderer": "invalid_renderer", "outputDir": "~/.pebblo"}
-    validator = config_validate.ReportsConfig(config)
+    validator = validate_config.ReportsConfig(config)
     validator.validate()
     assert validator.errors == [
         "Error: Unsupported renderer 'invalid_renderer' specified in the configuration"
@@ -78,7 +78,7 @@ def test_reports_config_validate():
         "renderer": "xhtml2pdf",
         "outputDir": "/non/existent/directory",
     }
-    validator = config_validate.ReportsConfig(config)
+    validator = validate_config.ReportsConfig(config)
     validator.validate()
     assert validator.errors == [
         "Error: Output directory '/non/existent/directory' specified for the reports does not exist"
@@ -96,7 +96,7 @@ def test_validate_config():
             "outputDir": "~/.pebblo",
         },
     }
-    config_validate.validate_config(config)
+    validate_config.validate_config(config)
     # If the configuration is valid, validate_config should not raise any exceptions
 
     # Test with invalid configuration
@@ -110,5 +110,5 @@ def test_validate_config():
         },
     }
     with pytest.raises(SystemExit):
-        config_validate.validate_config(config)
+        validate_config.validate_config(config)
     # If the configuration is invalid, validate_config should raise a SystemExit exception
